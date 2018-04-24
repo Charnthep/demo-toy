b2b2c37a908f849d027388fa98f93f7ef696ca97
blackwidow <blackwidow@blackwidows-Mac-mini.local>
2018-04-24 11:29:44 +0700

    Initial Project

---
+++ src/main/java/Workshop/Toy/ToyApplication.java
@@ -0,0 +1,12 @@
+package Workshop.Toy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyApplication.class, args);
+	}
+}
